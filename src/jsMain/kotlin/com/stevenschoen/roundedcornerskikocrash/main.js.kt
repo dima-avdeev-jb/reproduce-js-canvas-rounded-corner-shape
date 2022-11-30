02b90e786c8a42ffb186085c6ae1d7eb8cb76ab2
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-30 14:41:54 +0400

    change visual buttons

--- src/jsMain/kotlin/com/stevenschoen/roundedcornerskikocrash/main.js.kt
+++ src/jsMain/kotlin/com/stevenschoen/roundedcornerskikocrash/main.js.kt
@@ -1,5 +1,6 @@
 package com.stevenschoen.roundedcornerskikocrash
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,43 +8,52 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
-  onWasmReady {
-    Window("Hello") {
-      var shape by remember { mutableStateOf(RectangleShape) }
+    onWasmReady {
+        Window {
+            var shape by remember { mutableStateOf(RectangleShape) }
 
-      Column {
-        BasicText(
-          "RectangleShape",
-          Modifier.clickable { shape = RectangleShape }
-        )
-        BasicText(
-          "RoundedCornerShape(16.dp)",
-          Modifier.clickable { shape = RoundedCornerShape(16.dp) }
-        )
-        BasicText(
-          "RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)",
-          Modifier.clickable { shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp) }
-        )
-
-        Box(
-          Modifier
-            .size(100.dp)
-            .padding(16.dp)
-            .shadow(8.dp, shape)
-        )
-      }
+            Column {
+                DescribeButton(false, "RectangleShape") {
+                    shape = RectangleShape
+                }
+                DescribeButton(false, "RoundedCornerShape(16.dp)") {
+                    shape = RoundedCornerShape(16.dp)
+                }
+                DescribeButton(true, "RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)") {
+                    shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+                }
+                Box(
+                    Modifier
+                        .size(100.dp)
+                        .padding(16.dp)
+                        .shadow(8.dp, shape)
+                )
+            }
+        }
     }
-  }
+}
+
+@Composable
+fun DescribeButton(containsBug: Boolean, text: String, onClick: () -> Unit) {
+    val color: Color = if (containsBug) Color.Red else Color.Green
+    Box {
+        BasicText(
+            "CLICK ME ($text)",
+            Modifier
+                .clickable { onClick() }
+                .padding(10.dp)
+                .background(color.copy(alpha = 0.3f))
+                .padding(10.dp)
+        )
+    }
 }
